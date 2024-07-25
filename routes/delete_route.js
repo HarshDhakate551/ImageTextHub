0665baf3ce7664b7618660aafdce63b0223ab0a4
HarshDhakate551 <63715683+HarshDhakate551@users.noreply.github.com>
2024-07-25 23:50:09 +0530

    delete_route.js

--- routes/delete_route.js
+++ routes/delete_route.js
@@ -21,6 +21,8 @@ router.get("/DeleteAll_Data_Page", async (req, resp) => {
   const result = await Quote.deleteMany({});
   await Counter.findOneAndUpdate(
     // the id here is an unique id of persistant counter which increment, the value may varry in your case you thier may be chance that yiu have to update it
+    // to identify run the code once the couter collection would get created go to mongoose where in your database in counter collection their will be one field which 
+    // will contan the value in most cases the value should be same 
     { _id: "665338cee0e36ff7e7b9958b" },
     { $set: { counter: 0 } }
   );
