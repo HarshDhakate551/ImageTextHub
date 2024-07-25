1e080cfce1d087d89b7e6761c9851878bfb10459
HarshDhakate551 <63715683+HarshDhakate551@users.noreply.github.com>
2024-07-26 00:06:53 +0530

    delete_route.js

--- routes/delete_route.js
+++ routes/delete_route.js
@@ -20,9 +20,8 @@ router.get("/Delete_Perticular_Data_PAGE", async (req, resp) => {
 router.get("/DeleteAll_Data_Page", async (req, resp) => {
   const result = await Quote.deleteMany({});
   await Counter.findOneAndUpdate(
-    // the id here is an unique id of persistant counter which increment, the value may varry in your case you thier may be chance that yiu have to update it
-    // to identify run the code once the couter collection would get created go to mongoose where in your database in counter collection their will be one field which 
-    // will contan the value in most cases the value should be same 
+    // the id here is an unique id of persistant counter which increment, the value after running will contain 2 counter don't panik one was created so that the code
+    // could to reach to index Page as thier is some code which require counter thier so the bypass it i needed to create other counter
     { _id: "665338cee0e36ff7e7b9958b" },
     { $set: { counter: 0 } }
   );
