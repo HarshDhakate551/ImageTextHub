fba2f3f43966dab95db5399613464f4d392220bf
HarshDhakate551 <63715683+HarshDhakate551@users.noreply.github.com>
2024-07-25 23:58:55 +0530

    Home_page.js

--- routes/Home_page.js
+++ routes/Home_page.js
@@ -8,6 +8,14 @@ router.get("/", async (req, resp) => {
     // Randomly getting data from the deataset
     // Generating random number
     let indexFinding = await Counter.find({});
+
+    if (indexFinding.length === 0) {
+    // Initialize the counter if it doesn't exist
+    const initialCounter = new Counter({ counter: 1 });
+    await initialCounter.save();
+    indexFinding = [initialCounter];
+  }
+    
     let max = indexFinding[0].counter;
     let num = Math.floor(Math.random() * max + 1);
     console.log(num);
